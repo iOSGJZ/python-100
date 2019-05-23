f437d2a803d0095d59e953725907c8a881e7a8bd
guo jing zhe <541011933@qq.com>
2019-05-23 18:04:55 +0800

    新增示例

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import math
 import time
+import os
 
 from random import randint
 
@@ -59,3 +60,39 @@ def add(*args):
     for val in args:
         total += val
     return total
+
+#在屏幕上显示跑马灯文字
+def paomadeng():
+    content = '人生苦短，我用Python。Life is short, I use Python'
+    while True:
+        #清理屏幕,不同系统的命令不同
+        os.system('clear') #os.system('cls')
+        #打印内容
+        print(content)
+        #休眠200毫秒
+        time.sleep(0.2)
+        #通过字符串的切片，不断的把content的第一个字符去掉拼接到句尾，形成循环
+        content = content[1:] + content[0]
+
+#生成指定长度的验证码，验证码由大小写字符组成
+def generate_code(code_len=4):
+    """
+    生成指定长度的验证码
+
+    :param code_len: 验证码的长度(默认4个字符)
+
+    :return: 由大小写英文字母和数字构成的随机验证码
+    """
+    #所有可取的字符
+    all_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    #取随机下标的最大值
+    last_pos = len(all_chars) - 1
+    code = ''
+    #循环设定的长度的次数，每次随机一个下标取值拼接成code
+    for _ in range(code_len):
+        index = randint(0,last_pos)
+        code += all_chars[index]
+    return code
+
+
+
