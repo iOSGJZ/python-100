98443c6c00348dc84ac0dbcb9bf0e7a93ddb20c6
guo jing zhe <541011933@qq.com>
2019-05-23 10:32:48 +0800

    list

--- list.py
+++ list.py
@@ -1,3 +1,5 @@
+import sys
+
 def main():
     list1 = [1,3,5,6,100]
     print(list1)
@@ -48,5 +50,59 @@ def main():
     print(fruits5)
 
 
+def paixu():
+    list1 = ['orange','apple','zoo','internationalization','blueberry']
+    list2 = sorted(list1)
+    # sorted函数返回列表排序后的拷贝，不会修改传入的列表，默认根据首字母排序
+    #通过reverse关键字根据相反排序
+    list3 = sorted(list1,reverse=True)
+    #通过key关键字参数指定根据字符串长度进行排序而不是默认的字母表排序，从短到长
+    list4 = sorted(list1,key=len)
+    print(list1)
+    print(list2)
+    print(list3)
+    print(list4)
+    # 给列表对象发出排序消息直接在列表对象上进行排序
+    list1.sort(reverse=True)
+    print(list1)
+
+#用列表的生成式语法来创建列表
+def createList():
+    f = [x for x in range(1,10)]
+    print(f)
+    f = [x + y for x in 'ABCDE' for y in '1234567']
+    print(f)
+    #用列表的生成表达式语法创建列表容器
+    #用这种语法创建列表之后元素已经准备就绪所以需要耗费较多的内存空间
+    f = [x ** 2 for x in range(1,1000)]
+    print(sys.getsizeof(f)) #查看对象占用内存的字节数
+    print(f)
+    #注意使用（）创建的不是一个列表而是一个生成器对象
+    #通过生成器可以获取到数据但是不占用额外的空间存储数据
+    #每次需要数据的时候就通过内部的运算得到数据（需要花费额外的时间）
+    f = (x ** 2 for x in range(1,1000))
+    print(sys.getsizeof(f))# 相比生成式生成器不占用存储数据的空间
+    print(f)
+    for val in f:
+        print(val)
+"""
+Python中还有另外一种定义生成器的方式，
+就是通过yield关键字将一个普通函数改造成生成器函数。
+下面的代码演示了如何实现一个生成斐波拉切数列的生成器。
+所谓斐波拉切数列可以通过下面递归的方法来进行定义：
+"""
+
+def fib(n):
+    a,b = 0,1
+    for _ in range(n):
+        a,b = b, a + b 
+        yield a
+    
+
+
 if __name__ == "__main__":
     main()
+    paixu()
+    createList()
+    for val in fib(20):
+        print(val)
